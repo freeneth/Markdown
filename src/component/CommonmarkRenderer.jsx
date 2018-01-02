b915f6448f3e47eef1b44324d0e76f0f221e4a67
James Ruan <ruanbeihong@gmail.com>
2018-01-02 11:25:03 +0800

    renderer style:

--- src/component/CommonmarkRenderer.jsx
+++ src/component/CommonmarkRenderer.jsx
@@ -3,6 +3,42 @@ import React from 'react'
 /* eslint-enable */
 import { m } from '../style.js'
 import { Parser, HtmlRenderer } from 'commonmark'
+import styled from 'styled-components'
+
+/* eslint-disable no-unused-vars */
+const ContentDiv = styled.div`
+    max-width: 60em;
+    min-width: 30em;
+    margin: 0 auto;
+    padding: 2.5em;
+    h1, h2, h3, h4, h5, h6 {
+        line-height: 2em;
+    }
+    p, li {
+        line-height: 1.5em;
+    }
+    img {
+        max-width: 100%;
+    }
+    pre, xmp, plaintext, listing {
+        padding: 1em;
+        background-color: #dfebf5;
+    }
+    tt, code, kbd, samp {
+        background-color: #dfebf5;
+    }
+    a {
+        color: #0366d6;
+        text-decoration: none;
+    }
+    a:hover {
+        color: #1a03d6;
+    }
+    a:active {
+        color: #ba23d6;
+    }
+`
+/* eslint-enable */
 
 export default class CommonmarkRenderer extends React.PureComponent {
     constructor(props) {
@@ -20,13 +56,15 @@ export default class CommonmarkRenderer extends React.PureComponent {
 
         return <div
             style={m(CommonmarkRenderer.styles, styles)}
-            dangerouslySetInnerHTML={{ __html: html }}
-        />
+        >
+            <ContentDiv
+                dangerouslySetInnerHTML={{ __html: html }}
+            />
+        </div>
     }
 }
 
 CommonmarkRenderer.styles = {
-    background: '#f0f0f0',
-    padding: '1em',
+    background: '#fcfdf5',
     overflowY: 'auto',
 }
