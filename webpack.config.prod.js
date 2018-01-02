d3ba17cabe3da51471011dbbdf29c408692b1863
James Ruan <ruanbeihong@gmail.com>
2018-01-02 18:37:59 +0800

    pack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,17 +3,10 @@ const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
     entry: {
         main: path.resolve('./src/main.jsx'),
-    },
-    externals: {
-        immutable: 'Immutable',
-        react: 'React',
-        'react-dom': 'ReactDom',
-        'react-simple-file-list': 'SimpleFileList',
     },
     output: {
         path: __dirname,
@@ -22,10 +15,6 @@ module.exports = merge(common, {
         libraryTarget: 'umd',
     },
     plugins: [
-        new UglifyJSPlugin({
-            parallel: true,
-            sourceMap: true,
-        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
