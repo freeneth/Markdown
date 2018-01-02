dbb5595f9d2fe26cd60da25b6866b0a042b128a5
James Ruan <ruanbeihong@gmail.com>
2018-01-02 13:50:17 +0800

    save when editor text change

--- src/component/EditorController.jsx
+++ src/component/EditorController.jsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import PlainTextEditor from './PlainTextEditor.jsx'
 /* eslint-enable */
-import { ContentState, EditorState } from 'draft-js'
 
 
 export default class EditorController extends React.Component {
@@ -14,24 +13,29 @@ export default class EditorController extends React.Component {
         //console.log('EditorController componentDidMount')
     }
 
-    componentWillReceiveProps(nextProps){   
+    componentWillReceiveProps(nextProps){
         const { fileid, FileIOCmd } = this.props
         const nextFileid = nextProps.fileid
         if (fileid !== nextFileid) {
             FileIOCmd.pull(nextFileid)
-        }     
+        }
     }
 
     onChange(editorState) {
         const { updateEditor, fileid, FileIOCmd } = this.props
         updateEditor(editorState)
-        FileIOCmd.push(fileid)
+        const text = this.props.file.editor.getCurrentContent().getPlainText()
+
+        const newText = editorState.getCurrentContent().getPlainText()
+        if (text !== newText) {
+            FileIOCmd.push(fileid)
+        }
     }
 
     render() {
         const {file,styles} = this.props
         return (
-            <PlainTextEditor 
+            <PlainTextEditor
                 state={file.editor}
                 onChange={this.onChange.bind(this)}
                 styles={styles}
