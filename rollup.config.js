43ca4628ada48c9db84a8a34101268507cd2da72
James Ruan <ruanbeihong@gmail.com>
2018-01-24 17:22:21 +0800

    pack share to another file

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,27 @@ import cjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 
-export default {
+const externals = [
+    'babel-polyfill',
+    'commonmark',
+    'immutable',
+    'react',
+    'react-dom',
+    'react-modal2',
+    'react-redux',
+    'react-simple-file-list',
+    'redux',
+    'redux-logger',
+    'redux-saga',
+    'redux-saga/effects',
+    'slate',
+    'slate-plain-serializer',
+    'slate-react',
+    'styled-components',
+]
+
+
+export default [{
     input: 'src/main.jsx',
     output: {
         file: 'main.esm.js',
@@ -23,22 +43,25 @@ export default {
             'process.env.NODE_ENV': JSON.stringify( 'production' ),
         }),
     ],
-    external: [
-        'babel-polyfill',
-        'commonmark',
-        'immutable',
-        'react',
-        'react-dom',
-        'react-modal2',
-        'react-redux',
-        'react-simple-file-list',
-        'redux',
-        'redux-logger',
-        'redux-saga',
-        'redux-saga/effects',
-        'slate',
-        'slate-plain-serializer',
-        'slate-react',
-        'styled-components',
+    external: externals,
+}, {
+    input: 'src/share_main.jsx',
+    output: {
+        file: 'share_main.esm.js',
+        format: 'es',
+    },
+    plugins: [
+        string({
+            include: 'src/*.raw',
+        }),
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        cjs(),
+        resolve(),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify( 'production' ),
+        }),
     ],
-}
+    external: externals,
+}]
