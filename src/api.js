a7ca1a02484d34ea9cc0f7b9269583d25c2502bf
lufei <905729088@qq.com>
2018-08-22 18:12:30 +0800

    8.22本地跑完成

--- src/api.js
+++ src/api.js
@@ -1,4 +1,15 @@
 import { initLAPI } from "@leither/l-api";
-export const G = {api:null,ip:null};
-G.api = initLAPI(location.origin+'/webapi/')//阿里云
+
+const api = initLAPI('http://localhost' + '/webapi/')//本地测试
+//const api = initLAPI('http://192.168.1.187' + '/webapi/')//异地测试阿里云
+//const api = initLAPI(location.origin + '/webapi/')//正式
+
+const loginP = api.login('admin', '123456', 'byname');
+
+export const G = {
+    api,
+    sidP: loginP.then(({sid}) => sid),
+    userIdP: loginP.then(({ user: { id }}) => id)
+};
+
 window.G = G;
--- src/store.js
+++ src/store.js
@@ -5,15 +5,18 @@ import File from './file.js'
 import FileList from './fileList.js'
 import FileShare from './fileShare.js'
 import SyncState from 'redux-sync-state'
+import { G } from './api.js';
 export function mock_saveFile(id, json) {
-    return new Promise((res) => {
+    return new Promise(async (res) => {
         if (id) {
             if (json === null) {
-                console.log('remove file', id)
-                window.localStorage.removeItem(id)
+                console.log('remove file', id);
+                await G.api.hDel(await G.sidP, await G.userIdP, 'file', id);
+               // window.localStorage.removeItem(id)
             } else if (json !== undefined) {
                 console.log('save file', json)
-                window.localStorage.setItem(id, json)
+                await G.api.hSet(await G.sidP, await G.userIdP, 'file', id,json);
+                //window.localStorage.setItem(id, json)
             }
         }
         setTimeout(res, 500)
@@ -21,36 +24,40 @@ export function mock_saveFile(id, json) {
 }
 
 export function mock_saveFileList(json) {
-    return new Promise((res) => {
+    return new Promise(async (res) => {
         if (json) {
             console.log('save fileList', json)
-            window.localStorage.setItem('cmList', json)
+            await G.api.hSet(await G.sidP, await G.userIdP, 'fileList', 'cmList',json);
+            //window.localStorage.setItem('cmList', json)
         }
         setTimeout(res, 500)
     })
 }
 
 export function mock_loadFile(id) {
-    return new Promise((res) => {
-        const file = window.localStorage.getItem(id)
+    return new Promise(async (res) => {
+        //const file = window.localStorage.getItem(id);
+        const file= await G.api.hGet(await G.sidP, await G.userIdP, 'file',id);
         setTimeout(() => res(file), 500)
     })
 }
 
 export function mock_loadFileList() {
-    return new Promise((res) => {
-        const fileList = window.localStorage.getItem('cmList')
+    return new Promise(async (res) => {
+        //const fileList = window.localStorage.getItem('cmList')
+        const fileList = await G.api.hGet(await G.sidP, await G.userIdP, 'fileList','cmList');
         console.log('load fileList', fileList)
         setTimeout(() => res(fileList), 500)
     })
 }
 
 export function mock_setShare(shareid, fileid, json) {
-    return new Promise((res) => {
+    return new Promise(async (res) => {
         if (json) {
             console.log('set share option', json)
 
-            let shareidListJson = window.localStorage.getItem('shareidList' + fileid)
+            //let shareidListJson = window.localStorage.getItem('shareidList' + fileid)
+            let shareidListJson =await G.api.hGet(await G.sidP, await G.userIdP, 'Share','shareidList' + fileid);
             let shareidList = JSON.parse(shareidListJson)
             if (!shareidList) {
                 shareidList = []
@@ -58,17 +65,21 @@ export function mock_setShare(shareid, fileid, json) {
             const t = shareidList.find(id => (id === shareid))
             if (!t) {
                 shareidList.push(shareid)
-                window.localStorage.setItem('shareidList' + fileid, JSON.stringify(shareidList))
+
+                // window.localStorage.setItem('shareidList' + fileid, JSON.stringify(shareidList))
+                await G.api.hSet(await G.sidP, await G.userIdP, 'Share', 'shareidList' + fileid,JSON.stringify(shareidList));
             }
-            window.localStorage.setItem(shareid, json)
+            //window.localStorage.setItem(shareid, json)
+            await G.api.hSet(await G.sidP, await G.userIdP, 'Share', shareid,json);
         }
         setTimeout(res, 500);
     })
 }
 
 export function mock_getShare(fileid) {
-    return new Promise(res => {
-        const shareidList = JSON.parse(window.localStorage.getItem('shareidList' + fileid))
+    return new Promise(async res => {
+        // const shareidList = JSON.parse(window.localStorage.getItem('shareidList' + fileid))
+        const shareidList = JSON.parse(await G.api.hGet(await G.sidP, await G.userIdP, 'Share', 'shareidList' + fileid))
         if (shareidList) {
             setTimeout(() => {
                 const list = shareidList.map((id) => {
@@ -89,8 +100,9 @@ export function mock_getFileid() {
 }
 
 export function mock_loadShareFile() {
-    return new Promise((res) => {
-        const filejson = window.localStorage.getItem('pafdpduq56')
+    return new Promise(async (res) => {
+        // const filejson = window.localStorage.getItem('pafdpduq56')
+        const filejson = await  G.api.hGet(await G.sidP, await G.userIdP, 'ShareFile', 'pafdpduq56');
         const file = JSON.parse(filejson)
         setTimeout(() => res(file.text), 500)
     })
