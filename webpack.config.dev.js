bad799a4e2b025f8445ae9f0519529a4dc4e25da
lufei <905729088@qq.com>
2018-08-27 17:07:43 +0800

    8.27初次完成

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,8 @@
 const path = require('path')
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+let webpack = require('webpack');
 module.exports = merge(common, {
     entry: {
         app: ['babel-polyfill', path.resolve('./src/app.jsx')],
@@ -12,5 +13,14 @@ module.exports = merge(common, {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js',
     },
+    plugins: [
+        new UglifyJSPlugin({
+            parallel: true,
+        }),
+        new webpack.DefinePlugin({
+            ISPRODUCTION: JSON.stringify(true),
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+    ],
     devtool: 'source-map',
 })
