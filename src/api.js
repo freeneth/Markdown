ca3efe5782ab3bae1ae695913e17c3c032345a44
lufei <905729088@qq.com>
2018-08-27 10:55:02 +0800

    8.27保存

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import { initLAPI } from "@leither/l-api";
 
-const api = initLAPI('http://localhost' + '/webapi/')//本地测试
+//const api = initLAPI('http://localhost' + '/webapi/')//本地测试
 //const api = initLAPI('http://192.168.1.187' + '/webapi/')//异地测试阿里云
-//const api = initLAPI(location.origin + '/webapi/')//正式
+const api = initLAPI(location.origin + '/webapi/')//正式
 
 const loginP = api.login('admin', '123456', 'byname');
 
